afeb64a65ef1ce4cbc7cd0af7ca996b4442b43cb
Sai Nikhil Paruchuri <paruchus@sheridancollege.ca>
2020-03-28 00:27:42 -0400

    added footer directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FooterComponent} from "./footer/footer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent,
+        FooterComponent
+    ],
   imports: [
     BrowserModule
   ],
