1b1763127f62ca4b5aeeaf4bd1e77681d435d1e2
Sai Nikhil Paruchuri <paruchus@sheridancollege.ca>
2020-03-27 23:13:49 -0400

    Changes in component.html and component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,33 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   header = 'Exercise 3';
-  image1: string;
-  image2: string;
-  image3: string;
-  alt1: string;
-  alt2: string;
-  alt3: string;
+  image: string;
+  alt: string;
 
 
   constructor() {
-    this.image1 = 'assets/images/dice/side1.png';
-    this.alt1 = 'cat image';
+    this.image = 'assets/images/cat.PNG';
+    this.alt = 'cat image';
 
-    this.image2 = 'assets/images/dice/side2.png';
-    this.alt2 = 'dog image';
-
-    this.image3 = 'assets/images/dice/side3.png';
-    this.alt3 = 'rabbit image';
   }
-  roll() {
-    this.image1 = `assets/images/dice/side${rand1}.png`;
-    this.alt1 = `side ${rand1}`;
 
-    this.image2 = `assets/images/dice/side${rand2}.png`;
-    this.alt2 = `side ${rand2}`;
 
-    this.image3 = `assets/images/dice/side${rand3}.png`;
-    this.alt3 = `side ${rand3}`;
+  cat() {
+    this.image = 'assets/images/cat.PNG';
+    this.alt = 'cat image';
+  }
+
+  dog() {
+    this.image = 'assets/images/dog.PNG';
+    this.alt = 'cat image';
+  }
+
+  rabbit() {
+    this.image = 'assets/images/rabbit.PNG';
+    this.alt = 'rabbit image';
   }
 
 
